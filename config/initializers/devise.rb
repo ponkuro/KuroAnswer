6535196094e29f2adc5f7f43e4047d403b73c402
Hiroyuki Kurose <pon.kurose.web@gmail.com>
2016-07-17 06:37:54 +0000

    HTML/CSSの修など

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '8d0ce623dab528d04eb5fab3aa1de5ab03c7d88e1eae4cbf1a7663cb87c49cf9fb820bba2489e577626a8fce7f52b5e7f6a5f734620e9784adf238c20bab33a5'
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
